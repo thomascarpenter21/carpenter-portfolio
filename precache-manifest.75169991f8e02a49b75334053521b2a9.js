c256a4faee2ad7b6f2042652636ff6856acf0fd2
Thomas  Carpenter <thomas.carpenter21@gmail.com>
2020-04-05 14:52:42 -0700

    Updates

---
+++ precache-manifest.75169991f8e02a49b75334053521b2a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8b4162e6dfaff5ef7ecdcfd6d2b120",
+    "url": "/carpenter-portfolio/index.html"
+  },
+  {
+    "revision": "8c1b194af80231a1552d",
+    "url": "/carpenter-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47a8e5a18379bf65c848",
+    "url": "/carpenter-portfolio/static/js/2.89564049.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carpenter-portfolio/static/js/2.89564049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1b194af80231a1552d",
+    "url": "/carpenter-portfolio/static/js/main.f869e946.chunk.js"
+  },
+  {
+    "revision": "5a20a12a780cd6291431",
+    "url": "/carpenter-portfolio/static/js/runtime-main.844da3bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carpenter-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
